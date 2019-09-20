c2d978ed1a08f35da94c2774a857931641bc80aa
KirinToudou <kirin.toudou@protonmail.com>
2019-09-21 00:23:31 +0800

    [ADD] 62.unique-paths.js

---
+++ 62.unique-paths.js
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode id=62 lang=javascript
+ *
+ * [62] Unique Paths
+ */
+/**
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ */
+var uniquePaths = function (m, n) {
+    const matrix = new Array(m).fill(new Array(n).fill(1));
+    for (let i = 1; i < m; i++) {
+        for (let j = 1; j < n; j++) {
+            matrix[i][j] = matrix[i - 1][j] + matrix[i][j - 1];
+        }
+    }
+    return matrix[m - 1][n - 1];
+};
