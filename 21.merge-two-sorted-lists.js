ee2d5e95afaba8a05d8c273287afc3949873ee3a
KirinToudou <kirin.toudou@protonmail.com>
2019-09-21 00:23:30 +0800

    [ADD] 21.merge-two-sorted-lists.js

---
+++ 21.merge-two-sorted-lists.js
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode id=21 lang=javascript
+ *
+ * [21] Merge Two Sorted Lists
+ */
+
+// function ListNode(val) {
+//     this.val = val;
+//     this.next = null;
+// }
+
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var mergeTwoLists = function (l1, l2) {
+    if (l1 === null)
+        return l2;
+    if (l2 === null)
+        return l1;
+    if(l1.val<l2.val){
+        l1.next=mergeTwoLists(l1.next,l2);
+        return l1;
+    }else{
+        l2.next=mergeTwoLists(l1,l2.next);
+        return l2;
+    }
+};
+
